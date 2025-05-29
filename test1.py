117f825786c93282a28a9d7d25da649256ab7686
Nirmal1901 <74503593+Nirmal1901@users.noreply.github.com>
2025-05-29 12:59:06 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ def merge_sort(arr):
     mid = len(arr) // 2
     left_half = merge_sort(arr[:mid])
     right_half = merge_sort(arr[mid:])
-
+    
     # Merge the sorted halves
     return merge(left_half, right_half)
 
